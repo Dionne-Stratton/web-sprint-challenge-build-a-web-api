a9afbc318a542c5a73abdb426a695dd5f0de7cd1
Dionne Stratton <mylife33@hotmail.com>
2020-12-03 23:21:15 -0600

    mvp

--- api/server.js
+++ api/server.js
@@ -1,7 +1,13 @@
-const express = require('express');
+const express = require("express");
 const server = express();
+const actionsRouter = require("./actions/actions-router");
+const projectsRouter = require("./projects/projects-router");
 
-// Complete your server here!
-// Do NOT `server.listen()` inside this file!
+server.use(express.json());
+server.use("/api/actions", actionsRouter);
+server.use("/api/projects", projectsRouter);
+server.get("/", (_, res) => {
+  res.send("Server is running.");
+});
 
 module.exports = server;
